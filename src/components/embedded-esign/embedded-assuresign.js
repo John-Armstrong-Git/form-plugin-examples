b57cbda09f452d18c829d69166724bc60f5f5aac
John-Armstrong-Git <john.armstrong.career@gmail.com>
2024-07-02 10:33:29 -0400

    no comment

--- src/components/embedded-esign/embedded-assuresign.js
+++ src/components/embedded-esign/embedded-assuresign.js
@@ -1,215 +1,226 @@
-// import {css, html, LitElement, styleMap, until} from 'https://cdn.jsdelivr.net/gh/lit/dist@2/all/lit-all.min.js';
-import {css, html, LitElement} from 'lit';
-import { styleMap } from 'lit/directives/style-map.js';
-import {until} from 'lit/directives/until.js';
+import {
+  css,
+  html,
+  LitElement,
+  styleMap,
+  until,
+} from 'https://cdn.jsdelivr.net/gh/lit/dist@2/all/lit-all.min.js';
+// import { css, html, LitElement } from 'lit';
+// import { styleMap } from 'lit/directives/style-map.js';
+// import { until } from 'lit/directives/until.js';
 
 export class EmbeddedAssureSign extends LitElement {
-    // Define scoped styles right with your component, in plain CSS
-    static styles = css`
-      :host {
-        height: 100%;
-        width: 100%;
-        display: block;
+  // Define scoped styles right with your component, in plain CSS
+  static styles = css`
+    :host {
+      height: 100%;
+      width: 100%;
+      display: block;
+    }
+
+    .frame {
+      display: inline-block;
+      height: 100%;
+      width: 100%;
+      background-color: transparent;
+      border: none;
+    }
+  `;
+
+  static properties = {
+    src: { type: String },
+    content: { type: String },
+    envelopeName: { type: String },
+    height: { type: String },
+    signerName: { type: String },
+    signerEmail: { type: String },
+    assureSignApiUsername: { type: String },
+    assureSignApiKey: { type: String },
+    assureSignApiUserEmail: { type: String },
+    assureSignTemplateId: { type: String },
+  };
+
+  static getMetaConfig() {
+    // plugin contract information
+    return {
+      controlName: 'Embedded AssureSign',
+      fallbackDisableSubmit: false,
+      description: 'IFrame component which can render AssureSign envelope',
+      iconUrl: 'pen',
+      groupName: 'Signature',
+      version: '1.3',
+      properties: {
+        height: {
+          type: 'string',
+          title: 'Height',
+          description: 'Height of the component',
+          defaultValue: '1000px',
+        },
+        envelopeName: {
+          type: 'string',
+          title: 'Envelope Name',
+          defaultValue: 'Envelope Name',
+        },
+        signerEmail: {
+          type: 'string',
+          title: 'Signer Email',
+          defaultValue: 'john.armstrong@nintex.com',
+        },
+        signerName: {
+          type: 'string',
+          title: 'Signer Name',
+          defaultValue: 'John Armstrong',
+        },
+        assureSignApiUsername: {
+          type: 'string',
+          title: 'AssureSign API Username',
+          defaultValue: 'john.armstrong_u5eRugJO',
+        },
+        assureSignApiKey: {
+          type: 'string',
+          title: 'AssureSign API Password',
+          defaultValue: '49FFVT9/6i5/R9OchwcZfessHkahXRnt',
+        },
+        assureSignApiUserEmail: {
+          type: 'string',
+          title: 'AssureSign API User Email',
+          defaultValue: 'john.armstrong(demo)@nintex.com',
+        },
+        assureSignTemplateId: {
+          type: 'string',
+          title: 'AssureSign template Id',
+          defaultValue: '2baecacb-fa5b-458c-973e-ae5801134b3e',
+        },
+      },
+      standardProperties: {
+        readOnly: true,
+        description: true,
+      },
+    };
+  }
+
+  async load() {
+    console.log('Starting... without stringify');
+    console.log('Constructing Authentication Body...');
+
+    const apiUserBody = {
+      request: {
+        apiUsername: this.assureSignApiUsername,
+        key: this.assureSignApiKey,
+        contextUsername: this.assureSignApiUserEmail,
+        sessionLengthInMinutes: 60,
+      },
+    };
+    console.log(apiUserBody);
+
+    const apiUserBodyString = JSON.stringify(apiUserBody);
+    console.log(apiUserBodyString);
+    console.log('Making Authentication Call...');
+
+    const response = await fetch(
+      'https://account.assuresign.net/api/v3.7/authentication/apiUser',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: apiUserBodyString,
+        // body: apiUserBody
       }
+    );
 
-      .frame {
-        display: inline-block;
-        height: 100%;
-        width: 100%;
-        background-color: transparent;
-        border: none;
+    console.log('Authentication complete...');
+    console.log(response);
+
+    const jsonResponse = await response.json();
+
+    console.log(jsonResponse);
+
+    const token = jsonResponse.result.token;
+
+    const submitBody = {
+      request: {
+        placeholders: [],
+        templates: [
+          {
+            templateID: this.assureSignTemplateId,
+            values: [
+              {
+                name: 'Envelope Name 2 ',
+                value: this.envelopeName,
+              },
+              {
+                name: 'Language',
+                value: 'en-US',
+              },
+              {
+                name: 'New Employee Name',
+                value: this.signerName,
+              },
+              {
+                name: 'New Employee Email',
+                value: this.signerEmail,
+              },
+            ],
+          },
+        ],
+      },
+    };
+
+    const submit = await fetch(
+      'https://www.assuresign.net/api/documentnow/v3.7/submit',
+      {
+        method: 'POST',
+        headers: {
+          Authorization: 'Bearer ' + token,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(submitBody),
       }
-    `;
-    
-    static properties = {
-        src: { type: String },
-        content: { type : String },
-        envelopeName: { type: String },
-        height: { type: String },
-        signerName: { type: String },
-        signerEmail: { type: String },
-        assureSignApiUsername: { type: String },
-        assureSignApiKey: { type: String },
-        assureSignApiUserEmail: { type: String },
-        assureSignTemplateId: { type: String }
-    }
-    
-    static getMetaConfig() {
-        // plugin contract information
-        return {
-            controlName: "Embedded AssureSign",
-            fallbackDisableSubmit: false,
-            description: "IFrame component which can render AssureSign envelope",
-            iconUrl: "pen",
-            groupName: 'Nintex Custom Plugins',
-            version: '1.3',
-            properties: {
-                height: {
-                    type: 'string',
-                    title: 'Height',
-                    description: 'Height of the component',
-                    defaultValue: '1000px'
-                },
-                envelopeName: {
-                    type: 'string',
-                    title: 'Envelope Name',
-                    defaultValue: 'Envelope Name'
-                },
-                signerEmail: {
-                    type: 'string',
-                    title: 'Signer Email',
-                    defaultValue: 'john.armstrong@nintex.com'
-                },
-                signerName: {
-                    type: 'string',
-                    title: 'Signer Name',
-                    defaultValue: 'John Armstrong'
-                },
-                assureSignApiUsername: {
-                    type: 'string',
-                    title: 'AssureSign API Username',
-                    defaultValue: 'john.armstrong_u5eRugJO'
-                },
-                assureSignApiKey: {
-                    type: 'string',
-                    title: 'AssureSign API Password',
-                    defaultValue: '49FFVT9/6i5/R9OchwcZfessHkahXRnt'
-                },
-                assureSignApiUserEmail: {
-                    type: 'string',
-                    title: 'AssureSign API User Email',
-                    defaultValue: 'john.armstrong(demo)@nintex.com'
-                },
-                assureSignTemplateId: {
-                    type: 'string',
-                    title: 'AssureSign template Id',
-                    defaultValue: '2baecacb-fa5b-458c-973e-ae5801134b3e'
-                }
-            },
-            standardProperties: {
-                readOnly: true,
-                description: true,
-            }
-        };
-    }
-    
-    async load() {
+    );
 
-        console.log("Starting... without stringify");
-        console.log("Constructing Authentication Body...");
+    const jsonSubmit = await submit.json();
 
-        const apiUserBody = {
-            "request": {
-              "apiUsername": this.assureSignApiUsername,
-              "key": this.assureSignApiKey,
-              "contextUsername": this.assureSignApiUserEmail,
-              "sessionLengthInMinutes": 60
-            }
-        };
-        console.log(apiUserBody);
+    const envelopeId = jsonSubmit.result.envelopeID;
 
-        const apiUserBodyString = JSON.stringify(apiUserBody);
-        console.log(apiUserBodyString);
-        console.log("Making Authentication Call...");
+    const signingLinks = await fetch(
+      'https://www.assuresign.net/api/documentnow/v3.7/envelope/' +
+        envelopeId +
+        '/signingLinks',
+      {
+        method: 'GET',
+        headers: {
+          Authorization: 'Bearer ' + token,
+          'Content-Type': 'application/json',
+        },
+      }
+    );
 
-        const response = await fetch('https://account.assuresign.net/api/v3.7/authentication/apiUser', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: apiUserBodyString
-            // body: apiUserBody
-        });
+    const jsonSigningLinks = await signingLinks.json();
 
-        console.log("Authentication complete...");
-        console.log(response);
-        
-        const jsonResponse = await response.json();
+    let styles = { height: this.height };
+    return html` <iframe
+      class="frame"
+      style=${styleMap(styles)}
+      allow="geolocation *; microphone; camera"
+      src=${jsonSigningLinks.result.signingLinks[0].url}
+    ></iframe>`;
+  }
 
-        console.log(jsonResponse);
+  constructor() {
+    super();
+    (this.envelopeName = 'Envelope Name'), (this.height = '900px');
+  }
 
-        const token = jsonResponse.result.token;
+  async connectedCallback() {
+    super.connectedCallback();
+    this.content = this.load();
+  }
 
-        const submitBody = {
-            "request": {
-                "placeholders": [],
-                "templates": [
-                    {
-                        "templateID": this.assureSignTemplateId,
-                        "values": [
-                            {
-                                "name": "Envelope Name 2 ",
-                                "value": this.envelopeName
-                            },
-                            {
-                                "name": "Language",
-                                "value": "en-US"
-                            },
-                            {
-                                "name": "New Employee Name",
-                                "value": this.signerName
-                                },
-                            {
-                                "name": "New Employee Email",
-                                "value": this.signerEmail
-                            }
-                        ]
-                    }
-                ]
-            }
-        }
-        
-        const submit = await fetch('https://www.assuresign.net/api/documentnow/v3.7/submit',
-        {
-            method: 'POST',
-            headers: {
-                'Authorization': 'Bearer ' + token,
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(submitBody)
-        });
-
-        const jsonSubmit = await submit.json();
-
-        const envelopeId = jsonSubmit.result.envelopeID;
-        
-        const signingLinks = await fetch('https://www.assuresign.net/api/documentnow/v3.7/envelope/'+ envelopeId +'/signingLinks',
-            {
-                method: 'GET',
-                headers: {
-                    'Authorization': 'Bearer ' + token,
-                    'Content-Type': 'application/json'
-                }
-            }
-        );
-
-        const jsonSigningLinks = await signingLinks.json();
-        
-        let styles = {height: this.height};
-        return html`
-            <iframe
-            class="frame"
-            style=${styleMap(styles)}
-            allow="geolocation *; microphone; camera"
-            src=${jsonSigningLinks.result.signingLinks[0].url}
-            ></iframe>`;
-    }
-    
-    constructor() {
-        super();
-        this.envelopeName = 'Envelope Name',
-        this.height = '900px'
-    }
-
-    async connectedCallback() {
-        super.connectedCallback();
-        this.content = this.load();
-    }
-
-    // Render the UI as a function of component state
-    render() {
-        return html`${until(this.content, html`<span>Loading...</span>`)}`
-    }
+  // Render the UI as a function of component state
+  render() {
+    return html`${until(this.content, html`<span>Loading...</span>`)}`;
+  }
 }
 
 // registering the web component.
